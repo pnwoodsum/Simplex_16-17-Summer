d21c8605e5afc7db7db32ebd6103391dcf427cd0
Peter Woodsum <pnw8434@rit.edu>
2017-06-21 14:40:31 -0400

    Resetting repository

--- E03_Repository/Main.h
+++
@@ -1,8 +0,0 @@
-/*----------------------------------------------
-Programmer: Alberto Bobadilla (labigm@gmail.com)
-Date: 2017/05
-----------------------------------------------*/
-#ifndef __MAIN_H_
-#define __MAIN_H_
-#include "AppClass.h"
-#endif //__MAIN_H_
